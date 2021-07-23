772bd39c949950bcdf1d0a1336f392ec1e706667
Collinpatrick01 <69222590+Collinpatrick01@users.noreply.github.com>
2021-07-23 10:08:54 +0300

    Add files via upload

---
+++ desition_making_If_condition.py
@@ -0,0 +1,11 @@
+#Making desition basing on the weather of the day
+print('Type Hot or Cold for each question')
+weather = str(input('Enter The weather of the day: '))
+#Conditional statement
+if weather =="Hot":
+    print('Its hot Drink water ')
+elif weather == "Cold" :
+   print('Its cold wear Jacket')
+else:
+    print('Otherwise enjoy the day')
+    fdsa
